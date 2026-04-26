c1089f7034dde9712dca5249ebfd55455e3eae5b
alialmedani <alimedani02@gmail.com>
2026-04-26 10:50:54 +0300

    Update project structure

---
+++ CoffeeLearn.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CoffeeLearn.Domain.Entities;
+
+namespace CoffeeLearn.Application.Interfaces;
+
+public interface IApplicationDbContext
+{
+	DbSet<Product> Products { get; }
+	Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ CoffeeLearn.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Emit;
+using CoffeeLearn.Application.Interfaces;
+using CoffeeLearn.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeLearn.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext, IApplicationDbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Product> Products => Set<Product>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Product>(entity =>
+		{
+			entity.Property(x => x.Name).HasMaxLength(200).IsRequired();
+			entity.Property(x => x.Price).HasColumnType("decimal(18,2)");
+		});
+
+		base.OnModelCreating(modelBuilder);
+	}
+}
